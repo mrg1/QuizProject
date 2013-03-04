f0079d8898241f2142f8185e517223d6ee7e8c8e
Fan-Hal <fanhal@stanford.edu>
2013-03-03 20:20:14 -0800

    fucking shit penis

--- QuizSite/src/user/LoginServlet.java
+++ QuizSite/src/user/LoginServlet.java
@@ -1,6 +1,8 @@
 package user;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -39,10 +41,30 @@ public class LoginServlet extends HttpServlet {
 		ServletContext sc = getServletContext();
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA");
+			password = hexToString(md.digest(password.getBytes()));
+		} catch (NoSuchAlgorithmException ignored) {}
+		
 		if (UserInfo.checkPassword(username, password))
 			request.getRequestDispatcher("welcome-success.jsp").forward(request, response);
 		else
 			request.getRequestDispatcher("information-incorrect.html").forward(request, response);
 	}
-
+	
+	/*
+	 Given a byte[] array, produces a hex String,
+	 such as "234a6f". with 2 chars for each byte in the array.
+	 (provided code)
+	*/
+	public static String hexToString(byte[] bytes) {
+		StringBuffer buff = new StringBuffer();
+		for (int i=0; i<bytes.length; i++) {
+			int val = bytes[i];
+			val = val & 0xff;  // remove higher bits, sign
+			if (val<16) buff.append('0'); // leading 0
+			buff.append(Integer.toString(val, 16));
+		}
+		return buff.toString();
+	}
 }
