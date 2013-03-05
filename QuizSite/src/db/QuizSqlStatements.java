d22dad4d4808e912db12da5369f4ae367d5d3bb0
Fan-Hal <fanhal@stanford.edu>
2013-03-04 21:29:31 -0800

    fixed some bugs

--- QuizSite/src/db/QuizSqlStatements.java
+++ QuizSite/src/db/QuizSqlStatements.java
@@ -9,7 +9,6 @@ public class QuizSqlStatements {
 	
 	//Friend shit
 	private final static String FRIENDS_TABLE = "friends";
-	private final static String USER_ID = "user";
-	public final static String SQL_GET_FRIENDS = "SELECT * FROM " + FRIENDS_TABLE + " WHERE " + USER_ID + "=?;";
+	public final static String SQL_GET_FRIENDS = "SELECT * FROM " + FRIENDS_TABLE + " WHERE " + "user=?;";
 	public final static String SQL_ADD_FRIEND = "INSERT INTO " + FRIENDS_TABLE + " VALUES (?, ?);";
 }
