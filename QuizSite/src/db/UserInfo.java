ccfb833435a94327f7e73e1aa20bb07d524bff5a
Michael Garland <mrg1@stanford.edu>
2013-03-08 16:41:25 -0800

    adds elapsed time to score

--- QuizSite/src/db/UserInfo.java
+++ QuizSite/src/db/UserInfo.java
@@ -411,7 +411,7 @@ public class UserInfo {
 			selectStatement.setInt(1, quizId);
 			ResultSet rs = selectStatement.executeQuery();
 			while(rs.next()){
-				result.add(new Score(rs.getInt(2), quizId, rs.getString(1)));
+				result.add(new Score(rs.getInt(2), quizId, rs.getString(1), rs.getLong(3)));
 			}
 		}
 	    catch (SQLException e) {
@@ -428,7 +428,7 @@ public class UserInfo {
 			selectStatement.setString(1, username);
 			ResultSet rs = selectStatement.executeQuery();
 			while(rs.next()){
-				result.add(new Score(rs.getInt(2), rs.getInt(1), username));
+				result.add(new Score(rs.getInt(2), rs.getInt(1), username, rs.getInt(3)));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -443,6 +443,7 @@ public class UserInfo {
 			addStatement.setString(1, s.getUsername());
 			addStatement.setInt(2, s.getQuizId());
 			addStatement.setInt(3, s.getScore());
+			addStatement.setInt(4, s.getElapsed());
 			addStatement.execute();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -516,7 +517,7 @@ public class UserInfo {
 			selectStatement.setString(1, username);
 			ResultSet rs = selectStatement.executeQuery();
 			while(rs.next()){
-				history.add(new Score(rs.getInt(3), rs.getInt(1),rs.getString(2)));
+				history.add(new Score(rs.getInt(3), rs.getInt(1),rs.getString(2), rs.getInt(4)));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -533,7 +534,7 @@ public class UserInfo {
 			selectStatement.setInt(2, quizId);
 			ResultSet rs = selectStatement.executeQuery();
 			while(rs.next()){
-				history.add(new Score(rs.getInt(1), quizId, username));
+				history.add(new Score(rs.getInt(1), quizId, username, rs.getInt(2)));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
