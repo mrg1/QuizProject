081f28005c3d565514c4f46ac9256231ae8cadc9
Michael Garland <mrg1@stanford.edu>
2013-03-14 19:58:00 -0700

    adds achievement checking for quiz creation

--- QuizSite/src/db/AchievementInfo.java
+++ QuizSite/src/db/AchievementInfo.java
@@ -1,5 +1,7 @@
 package db;
 
+import java.util.List;
+
 public class AchievementInfo {
 	public static final int PRACTICE_MAKES_ID = 0;
 	public static final int AM_AUTH_ID = 1;
@@ -7,6 +9,10 @@ public class AchievementInfo {
 	public static final int PROD_AUTH_ID = 3;
 	public static final int QUIZ_MACHINE_ID = 4;
 	public static final int THE_GREATEST_ID = 5;
+	
+	public static final int AM_AUTH_VALUE = 1;
+	public static final int PROF_AUTH_VALUE = 5;
+	public static final int PROD_AUTH_VALUE = 10;
 	
 	public static String getAchievement(int id) {
 		switch(id) {
@@ -26,4 +32,17 @@ public class AchievementInfo {
 		return "";
 	}
 	
+	public static void checkQuizCreationAchievments(String username) {
+		List<Integer> quizzes = UserInfo.getAuthorHistory(username);
+		List<Integer> achs = UserInfo.getAchievements(username);
+		if(quizzes.size() == AchievementInfo.AM_AUTH_VALUE && !achs.contains(AM_AUTH_ID)){
+			UserInfo.addAchievment(username, AchievementInfo.AM_AUTH_ID);
+		}
+		if(quizzes.size() == AchievementInfo.PROF_AUTH_VALUE && !achs.contains(PROF_AUTH_ID)){
+			UserInfo.addAchievment(username, AchievementInfo.PROF_AUTH_ID);
+		}
+		if(quizzes.size() == AchievementInfo.PROD_AUTH_VALUE && !achs.contains(PROD_AUTH_ID)){
+			UserInfo.addAchievment(username, AchievementInfo.PROD_AUTH_ID);
+		}
+	}
 }
